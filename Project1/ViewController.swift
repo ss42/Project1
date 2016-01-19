7aeae71609449ddf9b3519986735d47c8b85ad84
ss42 <sanjay.shrestha1988@gmail.com>
2016-01-19 17:18:42 +0000

    firstCommit
    only buttons connected
    -Need to connect Facebook sign in

---
+++ Project1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Sanjay Shrestha on 1/18/16.
+//  Copyright © 2016 Sanjay Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func registerButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func facebookSignInButtonPressed(sender: UIButton) {
+        
+    }
+
+}
+
