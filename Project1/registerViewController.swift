14721d39203f28306a0d12ea9f2d953045a594c1
ss42 <you@example.com>
2016-02-15 07:46:33 -0800

    Added registerViewController, fixed the constraints, and added back button

---
+++ Project1/registerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  registerViewController.swift
+//  Project1
+//
+//  Created by Sanjay Shrestha on 2/15/16.
+//  Copyright © 2016 Sanjay Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonPressed(sender: UIButton) {
+    }
+
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
